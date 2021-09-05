cad7e1045eea91e8253f60b6e6a75f14c47f8140
Chuks <chuksleo.mark@gmail.com>
2021-09-05 18:01:30 +0100

    update to social links

--- application/views/pages/build.php
+++ application/views/pages/build.php
@@ -4,7 +4,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
-                    <h1>Lets Build For You</h1>
+                    <h1>Let's Build For You</h1>
                 </div>
            </div>
        </div>
